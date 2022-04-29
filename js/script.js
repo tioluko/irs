5e2a59504616ae71e1ea8643de2622f72b6731a9
Luko <tioluko586@gmail.com>
2022-04-29 00:19:59 -0300

    improvement

--- js/script.js
+++ js/script.js
@@ -2,23 +2,24 @@
 function pop(a)
 {
 var img = document.getElementById("selecao");
-if (a != "classico" && document.getElementById("tclassico").style.display === "block") {
+if (a != "classico" && document.getElementById("tclassico").style.display === "flex") {
     document.getElementById("tclassico").style.display = "none";
 }
-if (a != "gourmet" && document.getElementById("tgourmet").style.display === "block") {
+if (a != "gourmet" && document.getElementById("tgourmet").style.display === "flex") {
     document.getElementById("tgourmet").style.display = "none";
 }
-if (a != "doce" && document.getElementById("tdoce").style.display === "block") {
+if (a != "doce" && document.getElementById("tdoce").style.display === "flex") {
     document.getElementById("tdoce").style.display = "none";
 }
-if (document.getElementById(`t${a}`).style.display === "block") {
+if (document.getElementById(`t${a}`).style.display === "flex") {
     document.getElementById(`t${a}`).style.display = "none";
+    document.getElementById('box').style.display = "none";
     img.src="";
 }
 else {
-    document.getElementById(`t${a}`).style.display = "block";
+    document.getElementById(`t${a}`).style.display = "flex";
+    document.getElementById('box').style.display = "block";
     img.src=`img/${a}.JPG`;
 }
 return false;
 }
-
